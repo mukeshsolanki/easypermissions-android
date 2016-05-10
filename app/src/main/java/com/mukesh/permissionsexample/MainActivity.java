91a27b716c952336dccf7932fb06e44d2cd3acee
Mukesh <mukesh@keepworks.com>
2016-05-11 01:01:25 +0530

    Example updated

--- app/src/main/java/com/mukesh/permissionsexample/MainActivity.java
+++ app/src/main/java/com/mukesh/permissionsexample/MainActivity.java
@@ -5,38 +5,113 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 import mukesh.com.permissions.AppPermissions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-  public static final String[] PERMISSIONS = {
-      Manifest.permission.READ_PHONE_STATE, Manifest.permission.RECEIVE_SMS,
-      Manifest.permission.READ_SMS, Manifest.permission.WRITE_EXTERNAL_STORAGE
+  private static final String[] ALL_PERMISSIONS = {
+      Manifest.permission.READ_SMS, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+      Manifest.permission.CAMERA
   };
+
+  private static final int CAMERA_REQUEST_CODE = 1;
+  private static final int SMS_REQUEST_CODE = 2;
+  private static final int STORAGE_REQUEST_CODE = 3;
+  private static final int ALL_REQUEST_CODE = 0;
+  private Button mStorageButton, mCameraButton, mSmsButton, mAllButton;
   private AppPermissions mRuntimePermission;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     mRuntimePermission = new AppPermissions();
-    mRuntimePermission.requestPermission(this, PERMISSIONS, 0);
+    initializeUI();
+    setListener();
+  }
+
+  private void setListener() {
+    mStorageButton.setOnClickListener(this);
+    mCameraButton.setOnClickListener(this);
+    mSmsButton.setOnClickListener(this);
+    mAllButton.setOnClickListener(this);
+  }
+
+  private void initializeUI() {
+    mStorageButton = (Button) findViewById(R.id.storage_button);
+    mCameraButton = (Button) findViewById(R.id.camera_button);
+    mSmsButton = (Button) findViewById(R.id.sms_button);
+    mAllButton = (Button) findViewById(R.id.all_button);
   }
 
   @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
       @NonNull int[] grantResults) {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    if (requestCode == 0) {
-      List<Integer> permissionResults = new ArrayList<>();
-      for (int grantResult : grantResults) {
-        permissionResults.add(grantResult);
-      }
-      if (permissionResults.contains(PackageManager.PERMISSION_DENIED)) {
-        Log.d("PermissionResult=>", "Denied");
+    switch (requestCode) {
+      case ALL_REQUEST_CODE:
+        List<Integer> permissionResults = new ArrayList<>();
+        for (int grantResult : grantResults) {
+          permissionResults.add(grantResult);
+        }
+        if (permissionResults.contains(PackageManager.PERMISSION_DENIED)) {
+          Toast.makeText(this, "All Permissions not granted", Toast.LENGTH_SHORT).show();
+        } else {
+          Toast.makeText(this, "All Permissions granted", Toast.LENGTH_SHORT).show();
+        }
+        break;
+      case CAMERA_REQUEST_CODE:
+        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+          Toast.makeText(this, "Camera Permissions not granted", Toast.LENGTH_SHORT).show();
+        } else {
+          Toast.makeText(this, "Camera Permissions granted", Toast.LENGTH_SHORT).show();
+        }
+        break;
+      case SMS_REQUEST_CODE:
+        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+          Toast.makeText(this, "SMS Permissions not granted", Toast.LENGTH_SHORT).show();
+        } else {
+          Toast.makeText(this, "SMS Permissions granted", Toast.LENGTH_SHORT).show();
+        }
+        break;
+      case STORAGE_REQUEST_CODE:
+        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+          Toast.makeText(this, "Storage Permissions not granted", Toast.LENGTH_SHORT).show();
+        } else {
+          Toast.makeText(this, "Storage Permissions granted", Toast.LENGTH_SHORT).show();
+        }
+        break;
+    }
+  }
+
+  @Override public void onClick(View v) {
+    if (v == mCameraButton) {
+      if (mRuntimePermission.hasPermission(this, Manifest.permission.CAMERA)) {
+        Toast.makeText(this, "Camera permission already given", Toast.LENGTH_SHORT).show();
       } else {
-        Log.d("PermissionResult=>", "Granted");
+        mRuntimePermission.requestPermission(this, Manifest.permission.CAMERA, CAMERA_REQUEST_CODE);
+      }
+    } else if (v == mStorageButton) {
+      if (mRuntimePermission.hasPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+        Toast.makeText(this, "Storage permission already given", Toast.LENGTH_SHORT).show();
+      } else {
+        mRuntimePermission.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            STORAGE_REQUEST_CODE);
+      }
+    } else if (v == mSmsButton) {
+      if (mRuntimePermission.hasPermission(this, Manifest.permission.READ_SMS)) {
+        Toast.makeText(this, "SMS permission already given", Toast.LENGTH_SHORT).show();
+      } else {
+        mRuntimePermission.requestPermission(this, Manifest.permission.READ_SMS, SMS_REQUEST_CODE);
+      }
+    } else if (v == mAllButton) {
+      if (mRuntimePermission.hasPermission(this, ALL_PERMISSIONS)) {
+        Toast.makeText(this, "All permission already given", Toast.LENGTH_SHORT).show();
+      } else {
+        mRuntimePermission.requestPermission(this, ALL_PERMISSIONS, ALL_REQUEST_CODE);
       }
     }
   }
